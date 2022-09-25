a9afd6fb2ca552c8fe9e37d34c9cb28a75ec71b5
hiroshikatakata <ktym141@ezweb.ne.jp>
2022-09-25 20:00:20 +0900

    [add]ログイン機能実装
    deviseを使用してログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'boards#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'user/:id', to: 'users/registrations#detail'
+    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
---
+++ db/migrate/20220925102212_create_boards.rb
@@ -0,0 +1,10 @@
+class CreateBoards < ActiveRecord::Migration[6.1]
+  def change
+    create_table :boards do |t|
+      t.string "title", null: false
+      t.text "body", null: false
+      t.string "image_name"
+      t.timestamps
+    end
+  end
+end
