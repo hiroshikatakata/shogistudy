94068b20a8012c30243ac9d83fd5c23c2f7a5585
hiroshikatakata <ktym141@ezweb.ne.jp>
2022-10-06 23:04:31 +0900

    [add]rubocop導入
    rubocopのチェックに基づいてコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-    user.password = SecureRandom.urlsafe_base64
+      user.password = SecureRandom.urlsafe_base64
     end
   end
 end
--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get boards_index_url
     assert_response :success
   end
